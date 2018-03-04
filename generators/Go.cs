8bde60d837f4e3ddf5cb86bae8b7ec55f90d371d
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-03-04 22:39:55 +0200

    added golang generator

--- generators/Go.cs
+++ generators/Go.cs
@@ -1,28 +1,83 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace CodeGen.generators
 {
 	public class GoGenerator : Generator
 	{
+		private const string ClassFormat = "type {0} struct {{{1}}}{2}{3}";
+		private string Indent { get; set; } = GeneratorConf.GetIndent(true, 4);
+		
 		public override Dictionary<string, string> Generate(Package pkg)
 		{
-			return new Dictionary<string, string>();
+			var data = new Dictionary<string, string>();
+			Indent = GeneratorConf.GetIndent(!pkg.UseSpaces, 4);
+			foreach (var @class in pkg.Classes)
+			{
+				data[@class.Name] = GenerateClass(@class) + "\n";
+			}
+
+			return data;
 		}
 
 		protected override string GenerateClass(Class @class)
 		{
-			return "";
+			string fields = "", methods = "", classes = "";
+			fields = @class.Fields?.Aggregate("\n" + fields, (current, field) => current + GenerateField(field) + "\n");
+			methods = @class.Methods?.Aggregate("\n\n" + methods,
+				(current, method) => current + "func (" + @class.Name + ") " + GenerateMethod(method) + "\n\n");
+			return string.Format(ClassFormat, @class.Name, fields, methods, classes);
 		}
 
 		protected override string GenerateField(Field field)
 		{
-			return "";
+			var result = Indent;
+
+			if (field.Access == "public")
+			{
+				field.Name = field.Name?.First().ToString().ToUpper() + field.Name?.Substring(1);
+			}
+
+			result += field.Name + " " + field.Type;
+
+			return result;
 		}
 
 		protected override string GenerateMethod(Method method)
 		{
-			throw new NotImplementedException();
+			var result = "";
+			if (method.Access == "public")
+			{
+				method.Name = method.Name?.First().ToString().ToUpper() + method.Name?.Substring(1);
+			}
+			result += method.Name + "(";
+
+			for (var i = 0; i < method.Parameters?.Length; i++)
+			{
+				result += method.Parameters[i].Name + " " + method.Parameters[i].Type;
+				if (i+1 < method.Parameters?.Length)
+				{
+					result += ", ";
+				}
+			}
+
+			result += ")";
+
+			if (method.Return != "")
+			{
+				result += " " + method.Return;
+			}
+
+			result += " {";
+
+			if (method.Return != "")
+			{
+				result += "\n" + Indent + "return nil\n";
+			}
+
+			result += "}";
+
+			return result;
 		}
 	}
 }
