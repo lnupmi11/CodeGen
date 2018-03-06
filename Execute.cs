36e97180d195515f17ce450c54b2a42be00abdb8
YuriyLisovskiy <yuralisovskiy98@gmail.com>
2018-03-06 12:42:37 +0200

    yaml deserialization, additional methods in Execute.cs

--- Execute.cs
+++ Execute.cs
@@ -1,5 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using CodeGen.generators;
+using CodeGen.parser;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
 
 namespace CodeGen
 {
@@ -8,6 +14,8 @@ namespace CodeGen
 	/// </summary>
 	public static class ExecuteConf
 	{
+		private const string OutputDir = "out";
+		
 		/// <summary>
 		/// The main flow of application
 		/// </summary>
@@ -28,6 +36,79 @@ namespace CodeGen
 				Console.WriteLine(comment, filename);
 				Console.WriteLine(item.Value);
 			}
+		}
+		
+		private static Package ParseFileByFormat(string body, string fileName)
+		{
+			Package pkg;
+
+			var fileFormat = Parser.GetFileFormat(fileName);
+
+			switch (fileFormat)
+			{
+				case "xml":
+					pkg = DeserializeXml(body);
+					break;
+				case "json":
+					pkg = DeserializeJson(body);
+					break;
+				case "yml":
+					pkg = DeserializeYaml(body);
+					break;
+				default:
+					throw new InvalidDataException("Invalid format of '" + fileName + "' file.");
+			}
+
+			return pkg;
+		}
+		
+		private static string GetSerializedData(string url, string fileName)
+		{
+			return url != "" ? Parser.Download(url) : Parser.Read(fileName);
+		}
+		
+		private static void PutDataToFiles(Dictionary<string, string> fileContextMap, string lang)
+		{
+			var ext = Parser.GetExtension(lang);
+			Directory.CreateDirectory(OutputDir);
+			foreach (var file in fileContextMap)
+			{
+				Parser.Write(OutputDir + "/" + file.Key + ext, file.Value);
+			}
+			Console.WriteLine("Generated successfully.");
+		}
+		
+		private static void PutDataToStdout(Dictionary<string, string> fileContextMap, string lang)
+		{
+			var ext = Parser.GetExtension(lang);
+			var comment = GeneratorConf.Languanges[lang].Comment;
+			var filename = "";
+			foreach (var file in fileContextMap)
+			{
+				if (comment != "")
+				{
+					filename = string.Format(comment, OutputDir, file.Key + ext);
+				}
+				Console.WriteLine(filename);
+				Console.WriteLine("\n{0}\n", file.Value);
+			}
+		}
+
+		private static Package DeserializeYaml(string body)
+		{
+			var deserializer = new Deserializer(namingConvention: new CamelCaseNamingConvention());
+			var pkg = deserializer.Deserialize<Package>(new StringReader(body));
+			return pkg;
+		}
+
+		private static Package DeserializeJson(string body)
+		{
+			return new Package();
+		}
+
+		private static Package DeserializeXml(string body)
+		{
+			return new Package();
 		}
 	}
 }
--- generators/Models.cs
+++ generators/Models.cs
@@ -3,27 +3,27 @@
 	/// <summary>
 	/// The structure that describes package. Contains classes and subpackages
 	/// </summary>
-	public struct Package
+	public class Package
 	{
 		/// <summary>
 		/// Represents the name of the package. Type: string 
 		/// </summary>
-		public string Name;
-		
+		public string Name { get; set; }
+
 		/// <summary>
 		/// Represents using of spaces or tabs. Type: boolean
 		/// </summary>
-		public bool UseSpaces;
+		public bool UseSpaces { get; set; }
 		
 		/// <summary>
 		/// Represents classes. Type: array of type Class
 		/// </summary>
-		public Class[] Classes;
+		public Class[] Classes { get; set; }
 		
 		/// <summary>
 		/// Represents subpackages. Type: array of type Package
 		/// </summary>
-		public Package[] Packages;
+		public Package[] Packages { get; set; }
 
 		/// <inheritdoc />
 		public override string ToString()
@@ -37,37 +37,37 @@
 	/// Contains name, array of fields, methods and subclasses,
 	/// parent class name, access specifier. Overrides ToString() method.
 	/// </summary>
-	public struct Class
+	public class Class
 	{
 		/// <summary>
 		/// Represents the name of the class. Type: string
 		/// </summary>
-		public string Name;
+		public string Name { get; set; }
 		
 		/// <summary>
 		/// Represents fields of the class. Type: array of type Field
 		/// </summary>
-		public Field[] Fields;
+		public Field[] Fields { get; set; }
 		
 		/// <summary>
 		/// Represents methods of the class. Type: array of type Method
 		/// </summary>
-		public Method[] Methods;
+		public Method[] Methods { get; set; }
 		
 		/// <summary>
 		/// Represents subclasses of the class. Type: array of type Class
 		/// </summary>
-		public Class[] Classes;
+		public Class[] Classes { get; set; }
 		
 		/// <summary>
 		/// Represents parent class name. Type: string
 		/// </summary>
-		public string Parent;
+		public string Parent { get; set; }
 		
 		/// <summary>
 		/// Represents access level of the class. Type: string
 		/// </summary>
-		public string Access;
+		public string Access { get; set; }
 
 		/// <inheritdoc />
 		public override string ToString()
@@ -84,17 +84,17 @@
 		/// <summary>
 		/// Represents the name of the variable. Type: string
 		/// </summary>
-		public string Name;
+		public string Name { get; set; }
 		
 		/// <summary>
 		/// Represents the type of the variable. Type: string
 		/// </summary>
-		public string Type;
+		public string Type { get; set; }
 		
 		/// <summary>
 		/// Represents default value of the varibale. Type: string
 		/// </summary>
-		public string Default;
+		public string Default { get; set; }
 	}
 
 	/// <inheritdoc />
@@ -106,17 +106,17 @@
 		/// <summary>
 		/// Represents access level of the field. Type: string
 		/// </summary>
-		public string Access;
+		public string Access { get; set; }
 		
 		/// <summary>
 		/// Denotes if field is constant or not. Type: boolean
 		/// </summary>
-		public bool Const;
+		public bool Const { get; set; }
 		
 		/// <summary>
 		/// Denotes if field is static or not. Type: boolean
 		/// </summary>
-		public bool Static;
+		public bool Static { get; set; }
 	}
 
 	/// <inheritdoc />
@@ -130,36 +130,36 @@
 	/// The structure that describes method. Contains name, return type, access level, const and static properties
 	/// and array of parameters 
 	/// </summary>
-	public struct Method
+	public class Method
 	{
 		/// <summary>
 		/// Represents the name of the method. Type: string
 		/// </summary>
-		public string Name;
+		public string Name { get; set; }
 		
 		/// <summary>
 		/// Represents return type of the method. Type: string
 		/// </summary>
-		public string Return;
+		public string Return { get; set; }
 		
 		/// <summary>
 		/// Represents access level of the method. Type: string
 		/// </summary>
-		public string Access;
+		public string Access { get; set; }
 		
 		/// <summary>
 		/// Denotes if the method is const or not. Type: boolean
 		/// </summary>
-		public bool Const;
+		public bool Const { get; set; }
 		
 		/// <summary>
 		/// Denotes if the mathod is static or not. Type: boolean
 		/// </summary>
-		public bool Static;
+		public bool Static { get; set; }
 		
 		/// <summary>
 		/// Represents parameters of the method. Type: array of type Parameter
 		/// </summary>
-		public Parameter[] Parameters;
+		public Parameter[] Parameters { get; set; }
 	}
 }
