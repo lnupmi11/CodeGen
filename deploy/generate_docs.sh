5aca798d22df750ed60238fa0b1f37cbf561c2ee
OHopiak <vorikus@gmail.com>
2018-03-11 18:01:53 +0000

    fixed deploy script

--- deploy/generate_docs.sh
+++ deploy/generate_docs.sh
@@ -66,8 +66,9 @@ echo "" > .nojekyll
 ################################################################################
 ##### Generate the Doxygen code documentation and log the output.          #####
 echo 'Generating Doxygen code documentation...'
+cat "$DOXYFILE" | sed -e '/INPUT /s/ = .*/ = ..\/../' > Doxyfile
 # Redirect both stderr and stdout to the log file AND the console.
-doxygen "$DOXYFILE" 2>&1 | tee doxygen.log
+doxygen 2>&1 | tee doxygen.log
 
 echo 'Moving manual docs to github pages'
 cp -v $TRAVIS_BUILD_DIR/*.md ./ || true
