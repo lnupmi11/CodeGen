c742955154620cf760325bec4017099fc4ba2508
OHopiak <vorikus@gmail.com>
2018-03-23 15:34:33 +0000

    fix doc file yet another time

--- deploy/generate_docs.sh
+++ deploy/generate_docs.sh
@@ -82,22 +82,24 @@ cp -v "$TRAVIS_BUILD_DIR/"*.md ./ || true
 # Check this by verifying that the html directory and the file html/index.html
 # both exist. This is a good indication that Doxygen did it's work.
 
-git add --all
-if [ -d "docs/html" ] && [ -f "docs/html/index.html" ] && ! git diff --exit-code --cached; then
+if [ -d "docs/html" ] && [ -f "docs/html/index.html" ]; then
     echo 'Uploading documentation to the gh-pages branch...'
     # Add everything in this directory (the Doxygen code documentation) to the
     # gh-pages branch.
     # GitHub is smart enough to know which files have changed and which files have
     # stayed the same and will only update the changed files.
+		git add --all
 
-    # Commit the added files with a title and description containing the Travis CI
-    # build number and the GitHub commit reference that issued this build.
-    git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
-    
-    # Force push to the remote gh-pages branch.
-    # The ouput is redirected to /dev/null to hide any sensitive credential data
-    # that might otherwise be exposed.
-    git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
+		if ! git diff --exit-code --cached; then
+			# Commit the added files with a title and description containing the Travis CI
+			# build number and the GitHub commit reference that issued this build.
+			git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
+			
+			# Force push to the remote gh-pages branch.
+			# The ouput is redirected to /dev/null to hide any sensitive credential data
+			# that might otherwise be exposed.
+			git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
+		fi
 else
     echo '' >&2
     echo 'Warning: No documentation (html) files have been found!' >&2
